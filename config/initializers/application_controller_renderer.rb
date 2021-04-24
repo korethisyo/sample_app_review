ddef6d0c9afa1cb8eb26660185995a7c34fc77c0
EC2 Default User <korethis0816@gmail.com>
2021-04-24 09:17:35 +0000

    [Update]review

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b3e0ccce1605d50259fd57decd26b45255ea1637de97119672dae41764436230dd5b0158d04240ee58b8711c082cbf1a64bfecc7908e955279cc6d9f8c04c1f1'
